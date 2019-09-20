40dd2d4bfc7c56ea5ba0368a66275b8b12b3446e
parmveersingh <parammann1996@gmail.com>
2019-09-20 09:32:27 +0530

    linear reg

--- Regression/linear_regression.py
+++ Regression/linear_regression.py
@@ -8,7 +8,7 @@ class LinearRegression:
 		ones=np.ones([x.shape[0],1],dtype=int)
 		self.x=np.append(x,ones,axis=1)
 		self.y=y
-		self.theta=np.random.randn(x.shape[1])
+		self.theta=np.random.randn(self.x.shape[0])
 
 
 	def return_theta(self):
@@ -18,6 +18,32 @@ class LinearRegression:
 
 	def cost_function(self):
 
-		h=np.matmul(self.x,self.theta)
-		print(h)
+		#h=np.matmul(self.x,self.theta)
+		h1= (self.x.T)*(self.theta)
+		#print(h)
+		print(h1)
+		self.j=(1/2*(self.x.shape[0]))*np.sum((h-self.y)**2)
+		return self.j
 
+
+	def gradient(self,iters,alpha):
+		
+		self.alpha_history = []
+		self.cost_history =[]
+
+		for i in range(iters):
+			h=np.matmul(self.x,self.theta) 
+			print("theta")
+			anys=np.sum(h-self.y)
+			print(sum(self.x[:,0]*anys))
+			print(self.theta)
+			
+			j=self.cost_function()
+			self.cost_history.append(j)
+			self.alpha_history.append(self.theta)
+			self.theta = self.theta-(alpha/(self.x.shape[0]))*(self.x.T.dot(h-self.y))
+			print("#########")
+			print((1/(self.x.shape[0]))*((h-self.y).T.dot(self.x)))
+		print(self.theta)
+		print(self.alpha_history)
+		print(self.cost_history)
--- Regression/main.py
+++ Regression/main.py
@@ -13,7 +13,7 @@ mp.scatter(data.iloc[:,:1].values,data.iloc[:,1].values)
 mp.xlabel('Years of Experience')
 mp.ylabel('Salary')
 mp.title('Salary Based on Experience')
-mp.show()
+#mp.show()
 
 
 # Shuffle a data 
@@ -38,3 +38,10 @@ test_y=fs_y[int(fs_y.size*0.7):,:]
 from linear_regression import LinearRegression
 ls = LinearRegression(train_x,train_y)
 ls.cost_function()
+
+iters=1000
+alpha=0.05
+
+#ls.gradient(iters,alpha)
+
+
