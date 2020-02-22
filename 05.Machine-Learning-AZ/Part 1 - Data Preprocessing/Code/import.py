bb2b5545076e71be85eaa0d00c29e1f0add14497
parmveersingh <parammann1996@gmail.com>
2020-02-22 20:18:28 +0530

    testing

--- 05.Machine-Learning-AZ/Part 1 - Data Preprocessing/Code/import.py
+++ 05.Machine-Learning-AZ/Part 1 - Data Preprocessing/Code/import.py
@@ -2,4 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as pt
 
-dataset = pd.read_csv('../Data-Preprocessing/Data.csv');
+dataset = pd.read_csv('../Data-Preprocessing/Data.csv')
+x = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
+
+test
